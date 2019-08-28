a3e92fa945f0c15b8f3086ecc838f3dc6635fa02
Aldo Ziflaj <aldoziflaj95@gmail.com>
2019-08-28 21:21:07 +0200

    add username index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_28_043834) do
+ActiveRecord::Schema.define(version: 2019_08_28_191927) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_08_28_043834) do
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
