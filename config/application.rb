cec40bfe418d63b5a2d76aab1e8de9c27327e41e
Aldo Ziflaj <aldoziflaj95@gmail.com>
2019-08-28 07:23:29 +0200

    Rename BookMe to Skeduler

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module BookMe
+module Skeduler
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
