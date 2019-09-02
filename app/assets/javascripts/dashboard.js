6d6f21020b0e0cafadbb69a59884875cb2f8d454
Aldo Ziflaj <aldoziflaj95@gmail.com>
2019-09-02 16:27:32 +0200

    Populate slot views

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,9 +1,47 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var tabs = document.querySelectorAll('.tabs');
+  loadData().then(() => populateViews());
+
+  const tabs = document.querySelectorAll('.tabs');
   M.Tabs.init(tabs);
 
-  var fabs = document.querySelectorAll('.fixed-action-btn');
+  const fabs = document.querySelectorAll('.fixed-action-btn');
   M.FloatingActionButton.init(fabs);
 });
 
+const loadData = async () => {
+  const free_slots_response = await fetch('/free_slots');
+  const upcoming_meetings_response = await fetch('/upcoming_meetings');
 
+  window.slots = {
+    upcoming: await upcoming_meetings_response.json(),
+    free: await free_slots_response.json(),
+  };
+};
+
+const populateViews = () => {
+  const slots = window.slots;
+
+  populateUpcomingMeetings(slots.upcoming);
+  populateFreeSlots(slots.free);
+};
+
+const populateUpcomingMeetings = (data) => {
+  const template = `<li>Meeting with <a href="mailto:$guest_email">$guest_email</a> at $time</li>`;
+  const dateOptions = {hour: '2-digit', minute: '2-digit', day: '2-digit', month: 'short', year: 'numeric'};
+
+  document.querySelector('ul.upcoming-meetings').innerHTML = data.map(item => {
+    const formattedDate = (new Date(item.scheduled_at)).toLocaleString('en-US', dateOptions)
+    return template.replace('$time', formattedDate)
+      .replace(/\$guest_email/g, item.guest_email)
+  }).join('');
+};
+
+const populateFreeSlots = (data) => {
+  const template = `<li>Free slot at $time</li>`;
+  const dateOptions = {hour: '2-digit', minute: '2-digit', day: '2-digit', month: 'short', year: 'numeric'};
+
+  document.querySelector('ul.free-slots').innerHTML = data.map(item => {
+    const formattedDate = (new Date(item.scheduled_at)).toLocaleString('en-US', dateOptions)
+    return template.replace('$time', formattedDate)
+  }).join('');
+};
--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -1,19 +1,15 @@
 class SlotsController < ApplicationController
+  respond_to :json
+
   def free
     slots = FreeSlotsForUser::Query.new(current_user).call
 
-    respond_to do |format|
-      format.html { render 'slots/_free', slots: slots }
-      format.json { render json: slots }
-    end
+    render json: slots
   end
 
   def upcoming
     slots = UpcomingSlotsForUser::Query.new(current_user).call
 
-    respond_to do |format|
-      format.html { render 'slots/_free', slots: slots }
-      format.json { render json: slots }
-    end
+    render json: slots
   end
 end
